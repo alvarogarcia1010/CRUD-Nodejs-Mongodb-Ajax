5850d66b7f8c8496a8c132e4c974a79b1a401fbb
alvarogarcia1010 <00111317@uca.edu.sv>
2018-12-01 16:18:05 -0600

    Funcionalidad de eliminar registro utilizando ajax lista

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,14 @@ $(document).ready(function(){
     $("#save-new").click(function(){
         createBuscador();
     });
+
+    $("#table-browser").on("click", ".edit", function(){
+        console.log("le di click a edit");
+    });
+
+    $("#table-browser").on("click", ".delete", function(){
+        deleteBuscador($(this).attr("data-id"));
+    });
 });
 
 function getBuscadores(){
@@ -18,38 +26,7 @@ function getBuscadores(){
         },
         success: function(response){
             response.buscadores.forEach(buscador => {
-                rowBuscador += `<tr>
-                                    <form class="form">
-                                    <th scope="row">${buscador._id}</th>
-                                    <td>
-                                        <p class="mb-0">${buscador.nombre}</p>
-                                        <div class="form-group mx-sm-3 mb-2  d-none">
-                                            <label for="nombre" class="sr-only">Password</label>
-                                            <input type="text" class="form-control form-nombre" name="nombre" placeholder="Nombre">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="mb-0">${buscador.version}</p>
-                                        <div class="form-group mx-sm-3 mb-2  d-none">
-                                            <label for="version" class="sr-only">Version Actual</label>
-                                            <input type="text" class="form-control form-version" name="version" placeholder="Version Actual">
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="mb-0">${buscador.company}</p>
-                                        <div class="form-group mx-sm-3 mb-2 d-none">
-                                            <label for="company" class="sr-only">Compañia</label>
-                                            <input type="text" class="form-control form-company" name="company" placeholder="Nombre">
-                                        </div>
-                                    </td>
-                                    <td class="text-center">
-                                        <button type="button" class="btn btn-primary  mx-2 edit"><i class="fas fa-edit font-2"></i></button>
-                                        <button type="button" class="btn btn-danger  mx-2 delete"><i class="fas fa-trash-alt font-2"></i></button>
-                                        <button type="button" class="btn btn-success d-none mx-2 update"><i class="fas fa-check-circle font-2"></i></button>
-                                        <button type="button" class="btn btn-danger d-none mx-2 cancel"><i class="fas fa-times-circle font-2"></i></button>
-                                    </td>
-                                    </form>
-                                </tr>`
+                rowBuscador += createRow(buscador);
             });
 
             $("tbody").html(rowBuscador);
@@ -86,6 +63,26 @@ function createBuscador(){
     })
 };
 
+function deleteBuscador(id){
+    $.ajax({
+        url: "api/Buscadoresweb/" + id,
+        type: "DELETE",
+        dataType: "json",
+
+        error: function(){
+            console.log("Hubo un error en la peticion")
+        },
+
+        success: function(response){
+            if(response.success){
+                getBuscadores();
+            }else{
+                console.log(response.message);
+            }
+        }
+    })
+};
+
 function createRow (data){
     var row = `<tr>
                     <form class="form">
@@ -113,8 +110,8 @@ function createRow (data){
                     </td>
                     <td class="text-center">
                         <button type="button" class="btn btn-primary  mx-2 edit"><i class="fas fa-edit font-2"></i></button>
-                        <button type="button" class="btn btn-danger  mx-2 delete"><i class="fas fa-trash-alt font-2"></i></button>
-                        <button type="button" class="btn btn-success d-none mx-2 update"><i class="fas fa-check-circle font-2"></i></button>
+                        <button type="button" class="btn btn-danger  mx-2 delete" data-id="${data._id}"><i class="fas fa-trash-alt font-2"></i></button>
+                        <button type="button" class="btn btn-success d-none mx-2 update" data-id="${data._id}><i class="fas fa-check-circle font-2"></i></button>
                         <button type="button" class="btn btn-danger d-none mx-2 cancel"><i class="fas fa-times-circle font-2"></i></button>
                     </td>
                     </form>
