8ecedda7755c8f0abe6e63eb96dfd573dd74c420
alvarogarcia1010 <00111317@uca.edu.sv>
2018-12-01 14:55:50 -0600

    Definiendo estructura basica para la ruta api

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 mongoose.Promise = global.Promise;
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
