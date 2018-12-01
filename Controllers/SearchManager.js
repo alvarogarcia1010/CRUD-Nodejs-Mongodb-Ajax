448c6abfd8a6030a0b7cbb31c50945ef9056c3c7
alvarogarcia1010 <00111317@uca.edu.sv>
2018-12-01 14:47:28 -0600

    Creando estructura del Controlador de Buscadores web

---
+++ Controllers/SearchManager.js
@@ -0,0 +1,24 @@
+var BuscadorWeb = require('../Models/BuscadorWeb');
+const SearchManager = {};
+
+SearchManager.create = (req, res, next) =>{
+
+};
+
+SearchManager.update = (req, res, next) =>{
+
+};
+
+SearchManager.delete = (req, res, next) =>{
+
+};
+
+SearchManager.getBuscador = (req, res, next) =>{
+
+};
+
+SearchManager.getBuscadores = (req, res, next) =>{
+
+};
+
+module.exports = SearchManager;
